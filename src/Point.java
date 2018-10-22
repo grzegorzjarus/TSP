adf233e0a2fce2d9c7c6029a604fc4eefc165817
grzesiekpwrwroc <grzegorz.jarus@gmail.com>
2018-10-22 02:04:38 +0200

    Add equals and contains methods

--- src/Point.java
+++ src/Point.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Point {
 
     public double getX() {
@@ -15,11 +17,24 @@ public class Point {
         this.x = x;
         this.y = y;
     }
-    public String toString(){
-        return String.valueOf(this.getX())+ " " + String.valueOf(this.getY());
+
+    public boolean equals(Point point) {
+        return this.getY() == point.getY() && this.getX() == point.getX();
     }
 
-    public static double distance(Point point1, Point point2){
-        return Math.sqrt((Math.pow(point1.getX()-point2.getX(),2)+ Math.pow(point1.getY()-point2.getY(),2)));
+    public static boolean contains(List<Point> list, Point point) {
+        boolean contains = false;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(point)) contains = true;
+        }
+        return contains;
+    }
+
+    public String toString() {
+        return String.valueOf(this.getX()) + " " + String.valueOf(this.getY());
+    }
+
+    public static double distance(Point point1, Point point2) {
+        return Math.sqrt((Math.pow(point1.getX() - point2.getX(), 2) + Math.pow(point1.getY() - point2.getY(), 2)));
     }
 }
